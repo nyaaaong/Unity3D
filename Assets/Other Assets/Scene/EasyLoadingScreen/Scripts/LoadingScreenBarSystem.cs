795ed405d45111b4f4e9a858b35e39f8e1c72412
nyaaaong <framew0rk@naver.com>
2023-10-23 21:55:40 +0900

    코드 정리

--- Assets/Other Assets/Scene/EasyLoadingScreen/Scripts/LoadingScreenBarSystem.cs
+++ Assets/Other Assets/Scene/EasyLoadingScreen/Scripts/LoadingScreenBarSystem.cs
@@ -1,7 +1,7 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadingScreenBarSystem : MonoBehaviour
 {
