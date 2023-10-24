14cddc97a1613595739e2bd291e4c4c9d69aa271
nyaaaong <framew0rk@naver.com>
2023-10-24 22:47:57 +0900

    버그 수정

--- Assets/Scripts/Data/DataManager.cs
+++ Assets/Scripts/Data/DataManager.cs
@@ -29,6 +29,9 @@ public class DataManager : Singleton<DataManager>
 
 	public static void ResetPlayerLevel()
 	{
+		if (!Inst || Inst.m_IngameCharacterData == null)
+			return;
+
 		Inst.m_IngameCharacterData[(int)Char_Type.Player].Level = 1;
 	}
 
@@ -58,7 +61,7 @@ public class DataManager : Singleton<DataManager>
 		if (value < 1)
 			value = 1 - value + 1;
 
-		return Mathf.RoundToInt(Inst.m_BuffData[idx].Multiplier * value) * 100 - 100; // float값을 int로 바꿔주면서 퍼센트로 바꿔준다
+		return Mathf.RoundToInt(Inst.m_BuffData[idx].Multiplier * value * 10f) * 10 - 100;
 	}
 
 	public static void AddBuffPercent(Ability_Type buff, float value)
@@ -71,9 +74,7 @@ public class DataManager : Singleton<DataManager>
 			value = 1 - value + 1;
 
 		Inst.m_BuffData[idx].Multiplier *= value;
-		// 곱연산 이후에는 Percent에 넣어줘야 하는데 퍼센트로 만들기 위해 100을 곱해준 후, int로 변환 및 반올림한다.
-		// 마지막으로 100을 빼준다. 이유는 버프가 없을 때에는 기본적으로 1f인데 이건 100%가 아닌 0%이기 때문이다.
-		Inst.m_BuffData[(int)buff].Percent = Mathf.RoundToInt(Inst.m_BuffData[idx].Multiplier) * 100 - 100;
+		Inst.m_BuffData[(int)buff].Percent = Mathf.RoundToInt(Inst.m_BuffData[idx].Multiplier * 10f) * 10 - 100; // 일의 자리 수는 반올림하게 해놨다.
 		++Inst.m_BuffData[(int)buff].Stack;
 	}
 
